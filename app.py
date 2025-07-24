a1bc32347359b1209c18bdba13a979f31f6c6086
TanushS32 <tanushgowd@gmail.com>
2025-07-24 16:02:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/forex-services")
+def forex_services():
+    return render_template("forex-services.html")
+
+@app.route("/financial-services")
+def financial_services():
+    return render_template("financial-services.html")
+
+@app.route("/faqs")
+def faqs():
+    return render_template("faqs.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
